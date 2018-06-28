8e3f069bca9249ce7f8ed9d7c32fce9a5d6b360e
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-06-28 13:37:02 +0900

    update

--- app.js
+++ app.js
@@ -6,8 +6,3 @@ function vibration(){
     alert('진동 result is ');
     // todo 안드로이드에서 vibration 값 받아서 웹 화면에 표시해야함
 }
-function back(){
-    //alert('back');
-    window.close();
-    // todo 웹을 나가서 앱으로 돌아가야함
-}
