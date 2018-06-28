94ede5c2fedd6ade6270936f23bf7efc05f7e9a9
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-06-28 11:16:26 +0900

    app.js : 5.html(test)파일과 관련있는 js파일

---
+++ app.js
@@ -0,0 +1,13 @@
+function sound(){
+    alert('소음 result is ');
+    // todo 안드로이드에서 sound 값 받아서 웹 화면에 표시해야함
+}
+function vibration(){
+    alert('진동 result is ');
+    // todo 안드로이드에서 vibration 값 받아서 웹 화면에 표시해야함
+}
+function back(){
+    //alert('back');
+    window.close();
+    // todo 웹을 나가서 앱으로 돌아가야함
+}
