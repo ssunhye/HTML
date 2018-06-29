66ae09abed7442222b1186ec045d6307b0c7cc90
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-06-29 16:48:18 +0900

    update

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ function vibration(){
     alert('진동 result is ');
     // todo 안드로이드에서 vibration 값 받아서 웹 화면에 표시해야함
 }
+function setMessage(arg) {
+		var mainFrame = document.getElementById('textMessageFromApp');
+		var createFrame = document.createElement("div");
+		createFrame.innerHTML = arg;
+		mainFrame.appendChild(createFrame);
+}
