51c505439de65056289d1443d14bcb44e3953a1d
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-02 13:42:24 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -13,28 +13,102 @@ function setMessage(arg) {
 	mainFrame.appendChild(createFrame);
 }
 
-time=0;
 function drawChart(value) {
+	var legends = ['sound'];
+	var seriesData = tui.util.map(tui.util.range(2), function (value, index) {
+		var name = legends[index];
+		var data = tui.util.map(tui.util.range(20), function () {
+			return getRandom(150, 200);
+		});
+		return {
+			name: name,
+			data: data
+		};
+	});
+	var baseNow = new Date();
+	var startSecond = baseNow.getSeconds() - seriesData[0].data.length - 1;
+	var categories = tui.util.map(seriesData[0].data, function (value, index) {
+		var hour = baseNow.getHours();
+		var minute = baseNow.getMinutes();
+		var second = startSecond + index;
 
-	time++;
-	val = time.toString();
-	value = value.toString();
-	//데이터 setting
-	var data = google.visualization.arrayToDataTable([
-		['date', 'values'],
-		['val', 'value']
-	]);
-	
-	//옵션 setting
+		if (second < 0) {
+			minute -= 1;
+		}
+
+		if (minute < 0) {
+			hour -= 1;
+		}
+		return makeDate(hour, minute, (startSecond + index));
+	});
+	var container = document.getElementById('chart-area');
+	var data = {
+		categories: categories,
+		series: seriesData
+	};
 	var options = {
-		title: 'Sound Meter',
-		curveType: 'function',
-		legend: {
-		position: 'bottom'
+		chart: {
+			width: 1160,
+			height: 540,
+			title: 'Concurrent users'
+		},
+		xAxis: {
+			title: 'seconds',
+			labelInterval: 3,
+			tickInterval: 'auto'
+		},
+		yAxis: {
+			title: 'users'
+		},
+		series: {
+			spline: true,
+			showDot: true,
+			shifting: true
+		},
+		tooltip: {
+			grouped: true
 		}
 	};
+	var chart = tui.chart.lineChart(container, data, options);
 
-	//차트그리기
-	var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-	chart.draw(data, options);
+	chart.on('load', function () {
+		var index = categories.length;
+		setInterval(function () {
+			var now = new Date();
+			var category = makeDate(now.getHours(), now.getMinutes(), now.getSeconds());
+			var values = [getRandom(150, 200), getRandom(150, 200)];
+
+			chart.addData(category, values);
+			index += 1;
+		}, 1000);
+	});
+}
+
+
+function getRandom(start, end) {
+    return start + (Math.floor(Math.random() * (end - start + 1)));
+}
+
+function zeroFill(number) {
+    var filledNumber;
+
+    if (number < 10) {
+        filledNumber = '0' + number;
+    } else {
+        filledNumber = number;
+    }
+
+    return filledNumber;
+}
+
+function adjustTime(time, addTime) {
+    addTime = addTime || 60;
+    if (time < 0) {
+        time += addTime;
+    }
+    return time;
+}
+
+function makeDate(hour, minute, second) {
+    return zeroFill(adjustTime(hour, 24)) + ':' + zeroFill(adjustTime(minute)) + ':' + zeroFill(adjustTime(second));
 }
