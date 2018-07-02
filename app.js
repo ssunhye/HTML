33f149174469f664a2ac2ebbb0dad14eaf7b6084
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-02 10:39:30 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,8 +7,30 @@ function vibration(){
     // todo 안드로이드에서 vibration 값 받아서 웹 화면에 표시해야함
 }
 function setMessage(arg) {
-		var mainFrame = document.getElementById('textMessageFromApp');
-		var createFrame = document.createElement("div");
-		createFrame.innerHTML = arg;
-		mainFrame.appendChild(createFrame);
+	var mainFrame = document.getElementById('textMessageFromApp');
+	var createFrame = document.createElement("div");
+	createFrame.innerHTML = arg;
+	mainFrame.appendChild(createFrame);
+}
+
+function drawChart(value) {
+
+	//데이터 setting
+	var data = google.visualization.arrayToDataTable(value);
+	draw(data, {forceIFrame: false,
+                vAxis: {maxValue: 200}}
+    );
+
+	//옵션 setting
+	var options = {
+		title: 'Vibration Meter',
+		curveType: 'function',
+		legend: {
+		position: 'bottom'
+		}
+	};
+
+	//차트그리기
+	var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+	chart.draw(data, options);
 }
