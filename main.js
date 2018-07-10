f18059b0da90afbe65a188937eaf1ae39c8862c2
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-10 15:28:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function drawLoop( time ) {
     // draw a bar based on the current volume
     canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
 
-	document.getElementById("show").innerHTML = meter.volume;
+	document.getElementById("show").innerHTML = meter.volume*10000;
 	
     // set up the next visual callback
     rafID = window.requestAnimationFrame( drawLoop );
